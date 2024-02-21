fb8aade5638ce04cff16f33768a23b83e4453a59
Ed Whittle <ed.whittle@coles.com.au>
2024-02-21 13:31:08 +1100

    Adding match constructor

--- src/match/Match.ts
+++ src/match/Match.ts
@@ -1,6 +1,11 @@
 class Match {
+  private playerOne: string;
+  private playerTwo: string;
 
-    
-};
+  constructor(inputPlayerOne, inputPlayerTwo) {
+    this.playerOne = inputPlayerOne;
+    this.playerTwo = inputPlayerTwo;
+  }
+}
 
-export default Match
+export default Match;
